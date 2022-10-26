cc637be035d694cb731f3ced0fdec46714d0522d
Donnahue George <Donnahuegjr@gmail.com>
2022-10-26 16:03:20 -0400

    more fs testing

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const product = require("./api/product");
+const product = require('./api/product');
+const htmlRoutes = require('./home/index');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(express.json({ extended: false }));
-
-app.use("/api/product", product);
-
+app.use('/api/product', product);
+app.use('/', htmlRoutes);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
